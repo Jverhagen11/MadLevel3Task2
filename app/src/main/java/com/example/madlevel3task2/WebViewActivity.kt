82554b52bb8911b4a681db8e550f48c2394291ae
joel <joel_verhagen_1997@hotmail.com>
2020-09-28 16:42:18 +0200

    clickable recyclerview

---
+++ app/src/main/java/com/example/madlevel3task2/WebViewActivity.kt
@@ -0,0 +1,17 @@
+package com.example.madlevel3task2
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Created by akshaynandwana on
+ * 08, February, 2019
+ **/
+class WebViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_webview)
+    }
+}
